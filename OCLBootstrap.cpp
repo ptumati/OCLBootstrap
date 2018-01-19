295ddfb3505e9eae41bde498969692ff1f3db7ed
Pavan Tumati <pavan.github@tumati.net>
2018-01-19 12:31:19 -0500

    Learned I could use more than one device per context.

--- OCLBootstrap.cpp
+++ OCLBootstrap.cpp
@@ -24,8 +24,7 @@ int main( int argc, char *argv[] ) {
         std::cout << "Device Index [" << index++ << "] " << device.getInfo< CL_DEVICE_NAME >() << std::endl;
     }
 
-    cl::Context deviceOneContext( allDevices[0] );
-    cl::Context deviceTwoContext( allDevices[1] );
+    cl::Context clContext( allDevices );
     cl::Program::Sources sources;
 
     // calculates for each element; C = A + B
@@ -45,20 +44,14 @@ int main( int argc, char *argv[] ) {
         "           result[i] = A[i] * A[i];"
         "   }";
     sources.push_back({kernel_code.c_str(), kernel_code.length()});
-
-    cl::Program deviceOneProgram( deviceOneContext, sources );
-    cl::Program deviceTwoProgram( deviceTwoContext, sources );
+    cl::Program squareProgram( clContext, sources );
 
     auto buildProgram = []( cl::Program& program, cl::Device& device ) -> bool {
         return (program.build( {device} ) == CL_SUCCESS );
     };
 
-    if( !buildProgram( deviceOneProgram, allDevices[0] ) ) {
-        std::cout << "Error building: " << deviceOneProgram.getBuildInfo< CL_PROGRAM_BUILD_LOG >( allDevices[0] ) << std::endl;
-        return 1;
-    }
-    if( !buildProgram( deviceTwoProgram, allDevices[1] ) ) {
-        std::cout << "Error building: " << deviceTwoProgram.getBuildInfo< CL_PROGRAM_BUILD_LOG >( allDevices[1] ) << std::endl;
+    if( !buildProgram( squareProgram, allDevices[0] ) ) {
+        std::cout << "Error building: " << squareProgram.getBuildInfo< CL_PROGRAM_BUILD_LOG >( allDevices[0] ) << std::endl;
         return 1;
     }
 
@@ -66,17 +59,17 @@ int main( int argc, char *argv[] ) {
     int testArrayA[100] = {0};
     int resultArrayA[100] = {0};
     
-    auto openCLTestArrayABuffer = MakeBufferWrapper( testArrayA, deviceOneContext );
-    auto openCLTestArrayASizeBuffer = MakeBufferWrapper( testArrayASize, deviceOneContext );
-    auto openCLResultArrayBuffer = MakeBufferWrapper( resultArrayA, deviceOneContext );
-    
+    auto openCLTestArrayABuffer = MakeBufferWrapper( testArrayA, clContext );
+    auto openCLTestArrayASizeBuffer = MakeBufferWrapper( testArrayASize, clContext );
+    auto openCLResultArrayBuffer = MakeBufferWrapper( resultArrayA, clContext );
+
     for( int i = 0; i < 100; i++ ) testArrayA[i] = i;
 
-    cl::CommandQueue queue( deviceOneContext, allDevices[0] );
+    cl::CommandQueue queue( clContext, allDevices[0] );
     queue.enqueueWriteBuffer( openCLTestArrayABuffer, CL_TRUE, 0, openCLTestArrayABuffer.bufferSize(), testArrayA );
     queue.enqueueWriteBuffer( openCLTestArrayASizeBuffer, CL_TRUE, 0, openCLTestArrayASizeBuffer.bufferSize(), testArrayASize );
     
-    auto deviceOneKernel = cl::Kernel( deviceOneProgram, "square" );
+    auto deviceOneKernel = cl::Kernel( squareProgram, "square" );
     auto squareFunction =
       cl::KernelFunctor< cl::Buffer&, cl::Buffer&, cl::Buffer& >( deviceOneKernel );
 
@@ -84,6 +77,9 @@ int main( int argc, char *argv[] ) {
     squareFunction(cl::EnqueueArgs(queue,cl::NDRange(1)), openCLTestArrayABuffer, openCLTestArrayASizeBuffer, openCLResultArrayBuffer, error );
 
     queue.enqueueReadBuffer( openCLResultArrayBuffer, CL_TRUE, 0, openCLResultArrayBuffer.bufferSize(), resultArrayA );
+
+    for( auto i : resultArrayA ) std::cout << i << ' ';
+    std::cout << std::endl;
 
     return 0;
 };
